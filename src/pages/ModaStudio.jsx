f7629cd53b9653f037ce269173a6bfd9f9062d14
MarkRyuo <malupajhonmark3035@gmail.com>
2025-02-06 20:14:52 +0800

    Subject: Update

--- src/pages/ModaStudio.jsx
+++ src/pages/ModaStudio.jsx
@@ -1,7 +1,7 @@
 
 function ModaStudio() {
   return (
-    <div style={{width:"100%", height: "100vh", border: "1px solid black", display: "flex", justifyContent: "center", alignItems: "center"}}>
+    <div style={{width:"100%", height: "80vh", display: "flex", justifyContent: "center", alignItems: "center"}}>
       <h1 style={{fontSize: "3rem"}}
       >This Site is Under Construction<span>&#9888;</span>....</h1>
     </div>
