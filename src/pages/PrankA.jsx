03efb3a800718d1a1a226e3376695270eb8bca1f
MarkRyuo <malupajhonmark3035@gmail.com>
2025-02-09 21:15:00 +0800

    Subject: Update

--- src/pages/PrankA.jsx
+++ src/pages/PrankA.jsx
@@ -3,7 +3,7 @@
 function PrankA() {
   return (
     <div className="container-prank-A">
-      <img src="https://media.giphy.com/media/3o7aDq9463G008980Y/giphy.gif" alt="prank" />
+      <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExcjB1eXJydW0xbG5hcmRvaXF6NWVuNHUzMGVqd29ucWhoeWIzMnZxaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/4QxQgWZHbeYwM/giphy.gif" alt="prank" />
     </div>
   )
 }
